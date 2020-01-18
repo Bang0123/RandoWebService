d866245e9106a7b28507dc379d53a4081beac175
Bang0123 <nikolajbang1995@gmail.com>
2020-01-18 17:34:17 +0100

    Add all these templates to explore .netcore 3.1 and fsharp

---
+++ examplefsharpweb/WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace examplefsharpweb
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32 + (int (float this.TemperatureC / 0.5556))
